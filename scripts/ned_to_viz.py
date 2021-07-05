1e468a545b16eae0905fcb88899d0f4528dc236c
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2021-07-05 16:53:00 +0530

    Add comments and initial viz for runway idea

--- scripts/ned_to_viz.py
+++ scripts/ned_to_viz.py
@@ -8,34 +8,51 @@ from nav_msgs.msg import Odometry
 
 import numpy as np
 
+'''This class takes the odometry data from StateReader and converts it into appropirate XYZ format.
+Frome XPlane, the odometry X Y Z correspond to N E D values respectively. In order to make sense of the visualization, X is left as N, Y is made -E and Z is made -D.
+During visualization in rviz, X axis (red) corresponds to north, Y axis (green) corresponds to west and Z axis (blue) corresponds to up.
+'''
 class NEDToViz:
     def __init__(self):
         self.odomSub = rospy.Subscriber("/xplane/flightmodel/odom", Odometry, self.odom_callback)
         self.odomVizPub = rospy.Publisher("/xplane/flightmodel/odom_viz", Odometry, queue_size=10)
         rospy.Timer(period=rospy.Duration(0.1), callback=self.visualizer_update)
         self.odom_viz = Odometry()
+
+        '''Store initial position of aircraft to visualize runway
+        TODO : Add some conditions so that instead of the initial position, we have the info of the runway start ; Will be helpful if we spawn mid air
+        '''
+        self.initial_odom_viz = None
+        self.initialOdomVizPub = rospy.Publisher("/xplane/flightmodel/initial_odom_viz", Odometry, queue_size=10)
     
     def odom_callback(self, msg):
         self.odom_viz = msg
-        pn = (self.odom_viz.pose.pose.position.x) #/10.0
-        pe = (self.odom_viz.pose.pose.position.y) #/10.0
-        pd = (self.odom_viz.pose.pose.position.z) #/10.0
+        pn = (self.odom_viz.pose.pose.position.x) 
+        pe = (self.odom_viz.pose.pose.position.y) 
+        pd = (self.odom_viz.pose.pose.position.z) 
 
-        self.odom_viz.pose.pose.position.y = -pe
-        self.odom_viz.pose.pose.position.z = -pd
+        self.odom_viz.pose.pose.position.y = -pe # Y in rviz is west direction
+        self.odom_viz.pose.pose.position.z = -pd # Z in rviz is up direction
 
         quat = self.odom_viz.pose.pose.orientation
         (r,p,y) = tf.transformations.euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
-        p = -p
-        y = -y
+        p = -p # pitch orientation reverses
+        y = -y # yaw orientation reverses
         quat = tf.transformations.quaternion_from_euler(r,p,y)
         self.odom_viz.pose.pose.orientation.x = quat[0]
         self.odom_viz.pose.pose.orientation.y = quat[1]
         self.odom_viz.pose.pose.orientation.z = quat[2]
         self.odom_viz.pose.pose.orientation.w = quat[3]
+
+        '''Initial odom is taken in order to get a rough idea about the runway'''
+        if self.initial_odom_viz is None:
+            self.initial_odom_viz = self.odom_viz 
     
     def visualizer_update(self, event=None):
         self.odomVizPub.publish(self.odom_viz)
+
+        if self.initial_odom_viz is not None:
+            self.initialOdomVizPub.publish(self.initial_odom_viz)
 
 if __name__ == '__main__':
     rospy.init_node('xplane_ned_to_rviz')
