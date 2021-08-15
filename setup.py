7d7071bd7641d23ce383c88619b0158e8fc98582
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2021-08-15 19:23:21 +0530

    Add traj_x in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['coord_transforms', 'xpc'],
+    packages=['coord_transforms', 'xpc', 'trajx'],
     package_dir={'':'src'}
 )
 
