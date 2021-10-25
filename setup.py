9dbcf1d885dd029f14f91f3a75b0357429c7866c
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2021-10-25 12:05:15 +0530

    Add separate folder for xplane_ros utils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['coord_transforms', 'xpc', 'trajx'],
+    packages=['coord_transforms', 'xpc', 'traj_x', 'xplane_ros_utils'],
     package_dir={'':'src'}
 )
 
